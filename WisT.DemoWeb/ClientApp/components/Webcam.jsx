9e6fe668f9102c2faa061f685843cbedf911decf
avlo2000 <32394484+avlo2000@users.noreply.github.com>
2018-03-07 16:01:13 +0200

    1

--- WisT.DemoWeb/ClientApp/components/Webcam.jsx
+++ WisT.DemoWeb/ClientApp/components/Webcam.jsx
@@ -2,7 +2,28 @@
 import Webcam from 'react-webcam';
 
 export default class WebcamComponent extends React.Component {
+
+    setRef = (webcam) => {
+        this.webcam = webcam;
+    }
+
+    capture = () => {
+        const imageSrc = this.webcam.getScreenshot();
+        console.log(imageSrc);
+    };
+
     render() {
-        return <Webcam />;
+        return (
+            <div>
+                < Webcam
+                    audio={false}
+                    screenshotFormat="image/webp"
+                    ref={this.setRef}
+                    height={400}
+                    weight={400}
+                />
+                <button id="take" onClick={this.capture}>Take photo</button>
+            </div>
+        );
     }
 }

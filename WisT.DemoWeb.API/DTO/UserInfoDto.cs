ec4245dde2003c747bf4c3839ad14d58f58425f7
avlo2000 <32394484+avlo2000@users.noreply.github.com>
2018-04-26 14:55:02 +0300

    1

---
+++ WisT.DemoWeb.API/DTO/UserInfoDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace WisT.DemoWeb.API.DTO
+{
+    public class UserInfoDto
+    {
+        public string Login { get; set; }
+        public List<IFormFile> Photoes { get; set; }
+    }
+}

9a5694ac75c281ad7d207e2949baf72ebecc5990
Zelinskiy Oleksandr <sashko.zel2000@gmail.com>
2018-05-03 18:07:51 +0300

    dfghjkl;

---
+++ WisT.DemoWeb.API/Controllers/UploadUserInfoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using WisT.DemoWeb.API.DTO;
+
+namespace WisT.DemoWeb.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class UploadUserInfoController : Controller
+    {
+        [HttpPost]
+        public async Task Post(UserInfoDto userInfo)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                await userInfo.Photo.CopyToAsync(memoryStream);
+                var image = Image.FromStream(memoryStream);
+                var bitmap = new Bitmap(image);
+            }
+            var login = userInfo.Login;
+        }
+    }
+}
---
+++ WisT.DemoWeb.API/DTO/UserInfoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+
+namespace WisT.DemoWeb.API.DTO
+{
+    public class UserInfoDto
+    {
+        public string Login { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}

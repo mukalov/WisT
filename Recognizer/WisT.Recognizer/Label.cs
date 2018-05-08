7ca6d2532554bcfe1e92d36c4a4ba55ff02586e4
Dmitry Mukalov <mukalov@gmail.com>
2018-05-08 18:11:50 +0300

    Fixed structure

--- Recognizer/WisT.Recognizer/Label.cs
+++ Recognizer/WisT.Recognizer/Label.cs
@@ -8,17 +8,20 @@ namespace WisT.Recognizer.Identifier
         private IIdentifier _id;
 
         public string Name { get; set; }
-        public IIdentifier Id {
-                get {
-                    if (_id == null)
-                        throw new Exception("Id is not initialized");
-                    else
+        public IIdentifier Id
+        {
+            get
+            {
+                if (_id == null)
+                    throw new Exception("Id is not initialized");
+                else
                     return _id;
-                }
-                set {
-                    _id = value;
-                }
             }
+            set
+            {
+                _id = value;
+            }
+        }
 
         public Label(string Name)
         {

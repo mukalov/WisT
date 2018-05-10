c5de4be5210e7e19950fb46f84f8901cfbb83b1a
avlo2000 <32394484+avlo2000@users.noreply.github.com>
2018-05-10 12:50:27 +0300

    Registered some people to file storage

--- Recognizer/WisT.Tests/RecognizerTests.cs
+++ Recognizer/WisT.Tests/RecognizerTests.cs
@@ -20,7 +20,7 @@ namespace WisT.Recognizer.Identifier.Tests
             _testReco = new Recognizer(_imageRepo, _labelRepo);
 
             Bitmap load_img = new Bitmap(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName + @"\Recognizer\TestSample\TestPerson1\e2105db7-8e4f-4cbe-b1d0-7b0c7274431c.bmp");
-            _testImage = new FaceImage(load_img, _imageRepo.RepoPath + @"\Recognizer\haarcascade_frontalface_default.xml")
+            _testImage = new FaceImage(load_img, _imageRepo.RepoPath + @"\DTO\haarcascade_frontalface_default.xml")
             {
                 Id = new Identifier(0)
             }; // face detection there
@@ -43,7 +43,7 @@ namespace WisT.Recognizer.Identifier.Tests
             _testReco = new Recognizer(_imageRepo, _labelRepo);
 
             Bitmap load_img = new Bitmap(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName + @"\Recognizer\TestSample\TestPerson2\ea055021-3316-4a9f-bef4-1b5cebb428ad.bmp");
-            _testImage = new FaceImage(load_img, _imageRepo.RepoPath + @"\Recognizer\haarcascade_frontalface_default.xml")
+            _testImage = new FaceImage(load_img, _imageRepo.RepoPath + @"\DTO\haarcascade_frontalface_default.xml")
             {
                 Id = new Identifier(1)
             }; // face detection there
@@ -66,7 +66,7 @@ namespace WisT.Recognizer.Identifier.Tests
             _testReco = new Recognizer(_imageRepo, _labelRepo);
 
             Bitmap load_img = new Bitmap(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName + @"\Recognizer\TestSample\TestPerson3\bc23325e-a23c-45ac-b970-e26726eca09c.bmp");
-            _testImage = new FaceImage(load_img, _imageRepo.RepoPath + @"\Recognizer\haarcascade_frontalface_default.xml")
+            _testImage = new FaceImage(load_img, _imageRepo.RepoPath + @"\DTO\haarcascade_frontalface_default.xml")
             {
                 Id = new Identifier(2)
             }; // face detection there
@@ -89,7 +89,7 @@ namespace WisT.Recognizer.Identifier.Tests
             _testReco = new Recognizer(_imageRepo, _labelRepo);
 
             Bitmap load_img = new Bitmap(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName + @"\Recognizer\TestSample\TestPerson4\28e24e03-20cf-4c1b-bf7d-5d82d67d15f7.bmp");
-            _testImage = new FaceImage(load_img, _imageRepo.RepoPath + @"\Recognizer\haarcascade_frontalface_default.xml")
+            _testImage = new FaceImage(load_img, _imageRepo.RepoPath + @"\DTO\haarcascade_frontalface_default.xml")
             {
                 Id = new Identifier(3)
             }; // face detection there

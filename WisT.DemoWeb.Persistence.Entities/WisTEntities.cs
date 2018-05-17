5ec84b15e1b8b46cd414ba90f1b945a9073b7842
Maks Kurnosenko <kurnosenko.maksim@gmail.com>
2018-05-17 14:43:16 +0300

    Changed DatabaseFirst to CodeFirst

---
+++ WisT.DemoWeb.Persistence.Entities/WisTEntities.cs
@@ -0,0 +1,30 @@
+namespace WisT.DemoWeb.Persistence.DataEntities
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class WisTEntities : DbContext
+    {
+        public WisTEntities()
+            : base("name=WisTEntities")
+        {
+        }
+
+        public virtual DbSet<UserImage> UserImages { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<User>()
+                .HasMany(e => e.UserImages)
+                .WithRequired(e => e.User)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
